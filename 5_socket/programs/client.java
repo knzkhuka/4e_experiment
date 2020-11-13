8522a665ef95878094098ebf6285f46f1995f52a
knzkhuka <one235813@gmail.com>
2020-11-13 18:34:23 +0900

    cin,coutをインラインにした

--- 5_socket/programs/client.java
+++ 5_socket/programs/client.java
@@ -3,18 +3,16 @@ import java.io.*;
 import java.net.*;
 
 public class client {
-  public static String cin()throws IOException{return (new BufferedReader(new InputStreamReader(System.in))).readLine();}
-  public static void cout(String str){System.out.print(str);}
   public static void main(String[]args)throws IOException{
     String ip = "192.168.3.33";
     int port = 1145;
     Socket socket = new Socket(ip,port);
     while(true){
-      BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      String query = reader.readLine();
-      cout(query);
+      BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String query = socket_reader.readLine();
+      System.out.print(query);
       PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-      String line = cin();
+      String line = (new BufferedReader(new InputStreamReader(System.in))).readLine();
       writer.println(line);
       writer.flush();
       if(line.equals("end"))break;
