60069dd0e32d93c5543e72b6ce8cb206070ab2e1
knzkhuka <one235813@gmail.com>
2020-11-13 18:39:46 +0900

    clientもコメント追加

--- 5_socket/programs/client.java
+++ 5_socket/programs/client.java
@@ -8,13 +8,21 @@ public class client {
     int port = 1145;
     Socket socket = new Socket(ip,port);
     while(true){
+      // サーバーからのメッセージ(query)を受け取って標準出力(System.out)
       BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       String query = socket_reader.readLine();
       System.out.print(query);
-      PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-      String line = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-      writer.println(line);
-      writer.flush();
+      
+      //　標準入力から1行入力する
+      BufferedReader console_reader = new BufferedReader(new InputStreamReader(System.in));
+      String line = console_reader.readLine();
+
+      // 入力された文字列をサーバーに送信
+      PrintWriter socket_writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+      socket_writer.println(line);
+      socket_writer.flush();
+
+      // endなら通信終了なのでbreak
       if(line.equals("end"))break;
     }
     socket.close();
