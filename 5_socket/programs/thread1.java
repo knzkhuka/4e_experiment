bf95618050324ec14f17c006c835f458cc4334bc
knzkhuka <one235813@gmail.com>
2020-11-16 14:57:47 +0900

    remove test multi thread java

--- 5_socket/programs/thread1.java
+++
@@ -1,8 +0,0 @@
-public class thread1 extends Thread {
-  private int cnt;
-  public void run() {
-    System.out.println(getName()+" : "+cnt);
-    for(int i=0;i<10;++i)cnt+=i;
-    System.out.println(getName()+" : "+cnt);
-  }
-}
--- 5_socket/programs/thread2.java
+++
@@ -1,7 +0,0 @@
-public class thread2 extends Thread {
-  public void run() {
-    for (int i = 0; i < 10; ++i) {
-      System.out.println(getName() + " : " + i);
-    }
-  }
-}
