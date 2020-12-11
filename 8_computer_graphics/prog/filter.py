ca91abea9b5a0a6809f90013822e5e9ba9380061
knzkhuka <one235813@gmail.com>
2020-12-11 12:47:03 +0900

    ushisshi

--- 8_computer_graphics/prog/filter.py
+++ 8_computer_graphics/prog/filter.py
@@ -2,10 +2,14 @@ import cv2
 import numpy as np
 
 img = cv2.imread('tapu.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]], np.float32)
 kernel = np.ones((5, 5), np.float32) / 25
-img_filter = cv2.filter2D(img, -1, kernel)
+#img_filter = cv2.filter2D(img, -1, kernel)
+#img_filter = cv2.Laplacian(gray, cv2.CV_64F)
+#img_filter = cv2.bilateralFilter(img, 15, 20, 20)
+img_filter = cv2.Canny(gray, 15, 20, 20)
 
 cv2.imshow('tapu filetr', img_filter)
 cv2.waitKey(0)
