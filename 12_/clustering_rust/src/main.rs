1909de88a327a16b10b76aca30a8c772e815c3f8
knzkhuka <one235813@gmail.com>
2021-01-15 16:32:07 +0900

    hoboseikou

--- 12_/clustering_rust/src/main.rs
+++ 12_/clustering_rust/src/main.rs
@@ -1,4 +1,4 @@
-use gnuplot::{Caption, Color, Figure, PointSize, PointSymbol};
+use gnuplot::{Caption, Color, Figure};
 use std::error::Error;
 use std::io;
 use std::process;
@@ -9,9 +9,78 @@ struct Point {
   y: f64,
 }
 
+struct Unionfind {
+  par: Vec<usize>,
+  size: Vec<usize>,
+  tree_count: usize,
+}
+impl Unionfind {
+  fn new(n: usize) -> Unionfind {
+    let mut vec = vec![0; n];
+    for i in 0..n {
+      vec[i] = i;
+    }
+    Unionfind {
+      par: vec,
+      size: vec![1; n],
+      tree_count: n,
+    }
+  }
+  fn root(&mut self, k: usize) -> usize {
+    if k == self.par[k] {
+      k
+    } else {
+      let k_par = self.par[k];
+      let k_root = self.root(k_par);
+      self.par[k] = k_root;
+      k_root
+    }
+  }
+  fn same(&mut self, u: usize, v: usize) -> bool {
+    self.root(u) == self.root(v)
+  }
+  fn unite(&mut self, u: usize, v: usize) -> bool {
+    let mut u_par = self.root(u);
+    let mut v_par = self.root(v);
+    if u_par == v_par {
+      return false;
+    }
+    if self.size[u_par] < self.size[v_par] {
+      std::mem::swap(&mut u_par, &mut v_par);
+    }
+    self.par[v_par] = u_par;
+    self.size[u_par] += self.size[v_par];
+    self.tree_count -= 1;
+    true
+  }
+  fn root_set(&mut self) -> Vec<usize> {
+    let mut roots: Vec<usize> = Vec::new();
+    for i in 0..self.par.len() {
+      if i == self.par[i] {
+        roots.push(i)
+      }
+    }
+    roots
+  }
+  fn group(&mut self, r: usize) -> Vec<usize> {
+    let mut g: Vec<usize> = Vec::new();
+    for i in 0..self.par.len() {
+      if self.root(i) == r {
+        g.push(i);
+      }
+    }
+    g
+  }
+}
+
 fn read(data: &mut Vec<Point>) -> Result<(), Box<dyn Error>> {
   let mut rdr = csv::Reader::from_reader(io::stdin());
+  let mut head = 0;
   for result in rdr.records() {
+    if head == 0 {
+      head += 1;
+      continue;
+    }
     let record = result?;
     let x_str = &record[1];
     let y_str = &record[2];
@@ -22,33 +91,56 @@ fn read(data: &mut Vec<Point>) -> Result<(), Box<dyn Error>> {
   }
   Ok(())
 }
-/*
-fn calc_dist(a: Point, b: Point) -> f64 {
+
+fn calc_dist(a: &Point, b: &Point) -> f64 {
   let dx = a.x - b.x;
   let dy = a.y - b.y;
   (dx * dx + dy * dy as f64).sqrt()
 }
-*/
 
 fn main() {
-  let mut data: Vec<Point> = Vec::with_capacity(100);
+  let mut data: Vec<Point> = Vec::new();
   if let Err(err) = read(&mut data) {
     println!("error running example: {}", err);
     process::exit(1);
   }
-  let mut dat_x: Vec<f64> = Vec::new();
-  let mut dat_y: Vec<f64> = Vec::new();
-  for p in data {
-    dat_x.push(p.x);
-    dat_y.push(p.y);
+  let n: usize = data.len();
+  let mut edges: Vec<(f64, usize, usize)> = Vec::new();
+  for i in 0..n {
+    for j in i..n {
+      edges.push((calc_dist(&data[i], &data[j]), i, j));
+    }
   }
-  let limit_x = [0, 10];
-  let limit_y = [0, 10];
+  edges.sort_by(|a, b| a.partial_cmp(b).unwrap());
+  let m = 2;
+  let mut uni = Unionfind::new(n);
+  for (cost, u, v) in edges {
+    if uni.tree_count == m {
+      break;
+    }
+    uni.unite(u, v);
+  }
+  let R = uni.root_set();
+
+  let mut clusters: Vec<Vec<usize>> = Vec::new();
+  for r in R {
+    clusters.push(uni.group(r));
+  }
+  assert_eq!(clusters.len(), m);
+
+  let colors = ["red", "blue", "green", "black"];
   let mut fg = Figure::new();
   {
     let x = fg.axes2d();
-    x.points(&dat_x, &dat_y, &[Caption("sample points"), Color("red")]);
-    x.points(&limit_x, &limit_y, &[PointSymbol('+'), Color("black")]);
+    for i in 0..m {
+      let mut dat_x: Vec<f64> = Vec::new();
+      let mut dat_y: Vec<f64> = Vec::new();
+      for j in &clusters[i] {
+        dat_x.push(data[*j].x);
+        dat_y.push(data[*j].y);
+      }
+      x.points(&dat_x, &dat_y, &[Color(colors[i])]);
+    }
   }
   fg.set_terminal("png", "plot.png");
   let _ = fg.show();
