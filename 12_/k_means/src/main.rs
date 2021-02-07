450616dd421545bd72fc9907634c7216b377008b
knzkhuka <one235813@gmai.com>
2021-02-07 20:43:59 +0900

    plx pushgit add .git add .

--- 12_/k_means/src/main.rs
+++ 12_/k_means/src/main.rs
@@ -4,7 +4,19 @@ use std::io;
 use std::{collections::HashMap, process};
 use std::{error::Error, usize};
 
-fn main() {}
+fn main() {
+    let mut data = Vec::new();
+    if let Err(err) = read(&mut data) {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
+    let m = 2;
+    let clusers = k_means(data, m);
+}
+
+fn k_means(data: &mut Vec<Point>, m: usize) {
+    
+}
 
 #[derive(Debug, Copy, Clone)]
 struct Point {
